3226fc0de4479703cd80815e330788bfd9d925ce
Euan Goddard <euan.goddard@gmail.com>
2015-11-03 21:32:52 +0000

    Added sound effects to the game

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,32 @@
 var angular = require('angular');
 
-var app = angular.module('xmas-movies', [require('./services')]);
+var app = angular.module('xmas-movies', [
+    require('./services'),
+    require('./sound')
+]);
 
-app.controller('AppController', function (Normalizer, MOVIES) {
+app.config(function (SoundManagerProvider) {
+    SoundManagerProvider.set_sounds_root('/sounds/');
+});
+
+
+app.run(function (SoundManager) {
+    SoundManager.add_sounds(['hohoho']);
+});
+
+
+app.controller('AppController', function (Normalizer, SoundManager, MOVIES) {
     var ctrl = this;
     this.correct_movies = [];
 
     this.guess = function (movie) {
         var movie_normalized = Normalizer.normalize(movie);
+        
         if (MOVIES.indexOf(movie_normalized) > -1) {
             if (ctrl.correct_movies.indexOf(movie_normalized) > -1) {
                 alert('Already got ' + movie);
             } else {
+                SoundManager.play('hohoho');
                 ctrl.correct_movies.push(movie_normalized);
                 clear_guessed_movie();
             }
