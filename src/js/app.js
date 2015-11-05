d1c7cf2aefdfc73f7f4a48c663b2f33ea4433e9d
Euan Goddard <euan.goddard@gmail.com>
2015-11-05 21:07:48 +0000

    Added a magnifying glasses on touch enabled screens

--- src/js/app.js
+++ src/js/app.js
@@ -42,3 +42,42 @@ app.controller('AppController', function (SoundManager, MovieLookup) {
     };
     clear_guessed_movie();
 });
+
+
+app.directive('magnify', function () {
+    var directive = {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var magnifying_glass = angular.element('<div class="magnifying-glass off"/>');
+            element.after(magnifying_glass);
+            var magnifying_glass_half_size = 75;
+            
+            element.on('touchstart touchenter', function () {
+                scope.$apply(function () {
+                    magnifying_glass.removeClass('off');
+                });
+            });
+            
+            element.on('touchend touchleave touchcancel', function () {
+                scope.$apply(function () {
+                    magnifying_glass.addClass('off');
+                });
+            });
+            
+            element.on('touchmove', function (event) {
+                var touch = event.touches[0];
+                move_magnifying_glass(touch.clientX, touch.clientY); 
+            });
+            
+            var move_magnifying_glass = function (x, y) {
+                scope.$apply(function () {
+                    var parent = element.parent()[0];
+                    var left = x - magnifying_glass_half_size - parent.offsetLeft; 
+                    var top = y - magnifying_glass_half_size - parent.offsetTop;
+                    magnifying_glass.css({left: left + 'px', top: top + 'px', backgroundPositionX: -1 * left + 'px', backgroundPositionY: -1 * top + 'px'});
+                });
+            }
+        }
+    };
+    return directive;
+});
