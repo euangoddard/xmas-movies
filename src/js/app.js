71b35c89fb506dad3d41516b9e4f59f21446b52c
Euan Goddard <euan.goddard@gmail.com>
2015-11-16 20:33:36 +0000

    Switched from using lodash to Angular for string tempting

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var angular = require('angular');
-var template = require('lodash.template');
 
 
 var app = angular.module('xmas-movies', [
@@ -20,7 +19,7 @@ app.run(function (SoundManager) {
 });
 
 
-app.controller('AppController', function ($scope, SoundManager, MovieLookup, MOVIES) {
+app.controller('AppController', function ($scope, $interpolate, SoundManager, MovieLookup, MOVIES) {
     var ctrl = this;
     this.correct_movies = []//MOVIES;
     this.total_movies = MovieLookup.size;
@@ -29,16 +28,16 @@ app.controller('AppController', function ($scope, SoundManager, MovieLookup, MOV
         if (MovieLookup.has(movie_name)) {
             var movie = MovieLookup.get(movie_name);
             if (ctrl.correct_movies.indexOf(movie) > -1) {
-                var body_template = template('You already found "${name}". Try another.');
+                var body_template = $interpolate('You already found "{{ name }}". Try another.');
                 this.show_dialog('Already found', body_template(movie));
             } else {
                 SoundManager.play('hohoho');
                 ctrl.correct_movies.push(movie);
-                this.show_dialog('Correct!', template('You found "${name}!"')(movie));
+                this.show_dialog('Correct!', $interpolate('You found "{{ name }}!"')(movie));
                 clear_guessed_movie();
             }
         } else {
-            this.show_dialog('Not on the wall', template('"${name}" is not on the wall')({name: movie_name}));
+            this.show_dialog('Not on the wall', $interpolate('"{{ name }}" is not on the wall')({name: movie_name}));
         }
     };
     
@@ -64,7 +63,7 @@ app.controller('AppController', function ($scope, SoundManager, MovieLookup, MOV
     clear_guessed_movie();
     
     this.show_help_dialog = function () {
-        var body_template = template('On the wall you\'ll find ${size} Christmassy movies concealed in obscured posters. When you spot a movie you recognise, enter its name in the box at the bottom.');
+        var body_template = $interpolate('On the wall you\'ll find {{ size }} Christmassy movies concealed in obscured posters. When you spot a movie you recognise, enter its name in the box at the bottom.');
         this.show_dialog('Welcome to Christmas Movies', body_template(MovieLookup));
     };
     //this.show_help_dialog();
