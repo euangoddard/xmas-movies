b5df57a54adf98497026fbc4c8f88d6e9f3a9fb6
Euan Goddard <euan.goddard@gmail.com>
2015-11-28 13:21:50 +0000

    Added analytics tracking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var angular = require('angular');
+require('angulartics');
+
 
 var KEY_CODES_TO_IGNORE = [
     13, // return
@@ -13,6 +15,7 @@ var KEY_CODES_TO_IGNORE = [
 
 var app = angular.module('xmas-movies', [
     require('angular-animate'),
+    require('angulartics-google-analytics'),
     require('./services'),
     require('./sound')
 ]);
@@ -28,7 +31,7 @@ app.run(function (SoundManager) {
 });
 
 
-app.controller('AppController', function ($scope, $interpolate, $timeout, SoundManager, MovieLookup) {
+app.controller('AppController', function ($analytics, $scope, $interpolate, $timeout, SoundManager, MovieLookup) {
     var ctrl = this;
     this.correct_movies = [];
     this.total_movies = MovieLookup.size;
@@ -38,8 +41,9 @@ app.controller('AppController', function ($scope, $interpolate, $timeout, SoundM
             var movie = MovieLookup.get(movie_name);
             if (ctrl.correct_movies.indexOf(movie) > -1) {
                 SoundManager.play('wrong');
-                var body_template = $interpolate('You already found "{{ name }}". Try another.');
+                var body_template = $interpolate('You already found "{{ name }}"');
                 this.show_toast(body_template(movie));
+                $analytics.eventTrack('Already found', {category: 'Guess', label: movie.name});
             } else {
                 ctrl.correct_movies.push(movie);
                 clear_guessed_movie();
@@ -47,14 +51,17 @@ app.controller('AppController', function ($scope, $interpolate, $timeout, SoundM
                     SoundManager.play('hallelujah');
                     ctrl.show_dialog('You win!', $interpolate('You found the last movie, "{{ name }}". Thanks for playing and have a very Merry Christmas.')(movie));
                     ctrl.are_answers_shown = true;
+                    $analytics.eventTrack('Found all movies', {category: 'Guess'});
                 } else {
                     SoundManager.play('hohoho');
                     this.show_dialog('Correct!', $interpolate('You found "{{ name }}!"')(movie));
+                    $analytics.eventTrack('Correct guess', {category: 'Guess', label: movie.name});
                 }
             }
         } else {
             SoundManager.play('wrong');
             this.show_toast($interpolate('"{{ name }}" is not on the wall')({name: movie_name}));
+            $analytics.eventTrack('Incorrect guess', {category: 'Guess', label: movie_name});
         }
     };
     
@@ -72,7 +79,7 @@ app.controller('AppController', function ($scope, $interpolate, $timeout, SoundM
         this.toast.message = message;
         toast_timeout = $timeout(function () {
             ctrl.toast = {};
-        }, 7000);
+        }, 5000);
     };
     this.toast = {};
     
@@ -92,16 +99,19 @@ app.controller('AppController', function ($scope, $interpolate, $timeout, SoundM
     this.show_help_dialog = function () {
         var body_template = $interpolate('On the wall you\'ll find {{ size }} Christmassy movies concealed in obscured posters. When you spot a movie you recognise, enter its name in the box at the bottom.');
         this.show_dialog('Welcome to Christmas Movies', body_template(MovieLookup));
+        $analytics.eventTrack('Show help', {category: 'Game'});
     };
     
     this.show_credits_dialog = function () {
-        this.show_dialog('Credits', 'Card concept and implementation by Euan Goddard. Many thanks to Greg Jackson for visual design of the poster wall.'); 
+        this.show_dialog('Credits', 'Card concept and implementation by Euan Goddard. Many thanks to Greg Jackson for visual design of the poster wall.');
+        $analytics.eventTrack('Show credits', {category: 'Game'}); 
     };
     this.show_help_dialog();
 
     this.are_answers_shown = false;
     this.toggle_answers_visibility = function () {
         this.are_answers_shown = !this.are_answers_shown;
+        $analytics.eventTrack('Toggle answers', {category: 'Game'});
     };
 
     this.is_muted = SoundManager.is_muted;
@@ -109,9 +119,11 @@ app.controller('AppController', function ($scope, $interpolate, $timeout, SoundM
         if (this.is_muted) {
             SoundManager.is_muted = false;
             this.is_muted = false;
+            $analytics.eventTrack('Un-mute sound', {category: 'Game'});
         } else {
             SoundManager.is_muted = true;
             this.is_muted = true;
+            $analytics.eventTrack('Mute sound', {category: 'Game'});
         }
     };
 });
